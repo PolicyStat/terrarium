f4b9532b1ad1d0332b655738b97d83c9fe134fe2
Kyle Gibson <kyle.gibson@frozenonline.com>
2014-04-18 12:31:11 -0400

    Refs #49. Ignore restructured text files that start with _ when building table of contents

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ except ImportError:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', '_*.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
